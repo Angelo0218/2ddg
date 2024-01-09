d1304bdae7a30263306a4c9cd0ec13af746ec820
Angelo0218 <maplech0218@gmail.com>
2024-01-09 23:06:18 +0800

    'dsdas'

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1704812623|77029268";
+const CACHE_VERSION = "1704812754|207688355";
 const CACHE_PREFIX = "2D survivors-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
